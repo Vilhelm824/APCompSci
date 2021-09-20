2a454d432391947130f5c08782e141a978ebc66a
William Fischer <williamfischer22@js-student.org>
2021-09-20 15:19:55 -0700

    messed with parameters

--- AlgorithmicArt.py
+++ AlgorithmicArt.py
@@ -8,27 +8,27 @@ pendulum.speed(0)
 # parameter values for the pendulum
 # parameters for x axis
 freqX = 10
-ampX = 400
+ampX = 200
 phaseX = 0
-dampX = 0.5
-freqX1 = 8
-ampX1 = 100
+dampX = 0.01
+freqX1 = 5
+ampX1 = 200
 phaseX1 = 0
-dampX1 = 0.01
+dampX1 = 0.05
 # parameters for y axis
 freqY = 10
-ampY = 400
+ampY = 200
 phaseY = 0
-dampY = 0.5
+dampY = 0.01
 freqY1 = 10
-ampY1 = 100
+ampY1 = 200
 phaseY1 = 0
-dampY1 = 0.01
+dampY1 = 0.05
 
 # initial time
 startTime = time.time()
 
-for i in range(1000):
+for i in range(2000):
     timeElapsed = time.time() - startTime
 
     xPos = ampX*math.sin(timeElapsed*freqX + phaseX)*math.exp(-dampX*timeElapsed) + ampX1*math.sin(timeElapsed*freqX1 + phaseX1)*math.exp(-dampX1*timeElapsed)
