8f532bebd4d4fc14b55086c0c30a0602d2d98770
Will <willhfischer@gmail.com>
2021-09-21 16:38:09 -0700

    simplified function

--- AlgorithmicArt.py
+++ AlgorithmicArt.py
@@ -7,35 +7,25 @@ pendulum = turtle.Turtle()
 pendulum.speed(0)
 # parameter values for the pendulum
 # parameters for x axis
-freqX = 10
+freqX = 7
 ampX = 200
-phaseX = 0
+phaseX = math.pi/2 * 0
 dampX = 0.01
-freqX1 = 5
-ampX1 = 200
-phaseX1 = 0
-dampX1 = 0.05
 # parameters for y axis
-freqY = 10
+freqY = 4
 ampY = 200
 phaseY = 0
 dampY = 0.01
-freqY1 = 10
-ampY1 = 200
-phaseY1 = 0
-dampY1 = 0.05
 
 # initial time
 startTime = time.time()
-
-for i in range(2000):
+pendulum.pu()
+for i in range(5000):
     timeElapsed = time.time() - startTime
 
-    xPos = ampX*math.sin(timeElapsed*freqX + phaseX)*math.exp(-dampX*timeElapsed) + ampX1*math.sin(timeElapsed*freqX1 + phaseX1)*math.exp(-dampX1*timeElapsed)
-    yPos = ampY*math.sin(timeElapsed*freqY + phaseY)*math.exp(-dampY*timeElapsed) + ampY1*math.sin(timeElapsed*freqY1 + phaseY1)*math.exp(-dampY1*timeElapsed)
-    
+    xPos = ampX*math.sin(timeElapsed*freqX + phaseX)*math.exp(-dampX*timeElapsed) 
+    yPos = ampY*math.sin(timeElapsed*freqY + phaseY)*math.exp(-dampY*timeElapsed) 
     pendulum.goto(xPos, yPos)
-
-    print(xPos)
+    pendulum.pd()
 
 turtle.mainloop()
