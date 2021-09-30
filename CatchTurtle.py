084eaea2372be069a932e806c37fc52e5fcdce17
Will Fischer <willhfischer@icloud.com>
2021-09-30 14:27:41 -0700

    finished assignment

--- CatchTurtle.py
+++ CatchTurtle.py
@@ -1,6 +1,6 @@
 #-----import statements-----
 import turtle
-from random import randrange
+from random import choice, randrange
 
 #-----game configuration----
 screen = turtle.Screen()
@@ -9,16 +9,27 @@ turtleShape = "turtle"
 turtleSize = 2
 playerScore = 0
 fontSetup = ("Arial", 20, "normal")
+timer = 20
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
+colors = ["olive drab", "light pink", "dark orchid", "sienna", "chartreuse", "coral", "light green", "khaki"]
 #-----initialize turtle-----
 jeffery = turtle.Turtle()
 ScoreKeeper = turtle.Turtle()
+TimeKeeper = turtle.Turtle()
 jeffery.color(turtleColor)
 jeffery.shape(turtleShape)
 jeffery.turtlesize(turtleSize)
+jeffery.speed(9)
 jeffery.pu()
 ScoreKeeper.pu()
 ScoreKeeper.ht()
+ScoreKeeper.speed(0)
+TimeKeeper.pu()
+TimeKeeper.ht()
+TimeKeeper.speed(0)
 ScoreKeeper.goto(-400, 350)
+TimeKeeper.goto(350, 350)
 
 #-----game functions--------
 def ChangePosition():
@@ -35,11 +46,38 @@ def UpdateScore():
 
 
 def JeffWasClicked(x, y):
-    ChangePosition()
-    UpdateScore()
+    if not timer_up:
+        ChangePosition()
+        UpdateScore()
+        StampColor()
+        screen.bgcolor("medium spring green")
+    else:
+        jeffery.ht()
+        screen.bgcolor("magenta")
 
+
+
+def Countdown():
+  global timer, timer_up
+  TimeKeeper.clear()
+  if timer <= 0:
+    TimeKeeper.write("Time's Up", font=fontSetup)
+    timer_up = True
+  else:
+    TimeKeeper.write("Timer: " + str(timer), font=fontSetup)
+    timer -= 1
+    TimeKeeper.getscreen().ontimer(Countdown, counter_interval) 
+
+
+def StampColor():
+    global colors
+    jeffery.color(choice(colors))
+    jeffery.stamp()
+    jeffery.color(turtleColor)
 
 #-----events----------------
 jeffery.onclick(JeffWasClicked)
+TimeKeeper.getscreen().ontimer(Countdown, counter_interval)
 
 screen.mainloop()
+
