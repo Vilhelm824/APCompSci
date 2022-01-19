a82164051d352358d2aa1d1278c0219ab1ed2dbc
Will Fischer <willhfischer@icloud.com>
2022-01-19 14:59:49 -0800

    gooey situation x(

---
+++ GUIsituation/GUISituation.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+ 
+ 
+def test_my_button():
+    frame_auth.tkraise()
+ 
+ 
+# main window
+root = tk.Tk()
+root.wm_geometry("250x150")
+root.title("Authorization")
+
+# Add this code before the code that creates your "Login" button
+bt_image = tk.PhotoImage(file="button.gif")
+bt_image = bt_image.subsample(10,10)
+
+# create empty frame
+frame_login = tk.Frame(root)
+frame_login.grid(row=0, column=0, sticky='news')
+lbl_username = tk.Label(frame_login, text='Username:', font='Courier')
+lbl_username.pack()
+ent_username = tk.Entry(frame_login, bd=3)
+ent_username.pack(pady=5)
+lbl_password = tk.Label(frame_login, text='Password: ', font='Courier')
+lbl_password.pack()
+ent_password = tk.Entry(frame_login, bd=3, show='*')
+ent_password.pack(pady=5)
+btn_login = tk.Button(frame_login, text='Login', command=test_my_button, image=bt_image)
+btn_login.pack()
+
+frame_auth = tk.Frame(root)
+frame_auth.grid(row=0, column=0, sticky='news')
+ 
+frame_login.tkraise()
+ 
+root.mainloop()
