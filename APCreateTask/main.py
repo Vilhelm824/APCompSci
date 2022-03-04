d8a46f646e0923ee0227fbb1c0587273e5f532fa
Will Fischer <willhfischer@icloud.com>
2022-03-04 10:14:35 -0800

    more work

--- APCreateTask/main.py
+++ APCreateTask/main.py
@@ -1,19 +1,47 @@
 # Goal: CLI that displays info about an IP adress, sourced from an online API and parsed from JSON format
 import webbrowser
 import requests
+import ipaddress
+
+
+# use api to get infromation about the IP address and parse it into a dictionary
+def GetInfo(ipAddress):
+    # HTTP request ap-api.com for info on the IP and return data(selected with fields option) as JSON
+    # uses f-strings to add the query into the string
+    ipInfo = requests.get(f"http://ip-api.com/json/{ipAddress}?fields=status,message,country,regionName,city,lat,lon,org,query")
+    # convert the JSON data from the request into a python dictionary
+    return ipInfo.json()
+
+
+# print out data nicely
+def PrintOutput(addressInfo):
+    print("========================= \n||IP Adress Information|| \n=========================")
+    print("Country:", addressInfo['country'], "\nRegion/State:", addressInfo['regionName'], "\nCity:", addressInfo['city'])
+    print("Coordinates:", str(addressInfo['lat']) + ",", str(addressInfo['lon']))
+    print("Organization:", addressInfo['org'],"\n")
+
+
+# open web browser with location on maps
+def OpenMap(addressInfo):
+    latitude = addressInfo['lat']
+    longitude = addressInfo['lon']
+    webbrowser.open(f"https://www.google.com/maps/place/{latitude},{longitude}")
+
+
+# TODO: error messages and incorrect input
+def ValidateIP(ipToCheck):
+    try:
+        ipaddress.ip_address(ipToCheck)
+    except:
+        print("invalid ip")
 
 # IP address to check
-query = input("Enter IP Address to check (just hit enter for localhost): ")
+query = input("Enter IP Address (leave blank for localhost): ")
 
-# HTTP request ap-api.com for info on the IP and return data(selected with fields option) as JSON
-# uses f-strings to add the query into the string
-ipInfo = requests.get(f"http://ip-api.com/json/{query}?fields=status,message,country,regionName,city,zip,lat,lon,org,query")
+ValidateIP(query)
 
-# convert the JSON data from the request into a python dictionary
-parsedInfo = ipInfo.json()
-
-print(parsedInfo)
-
-# TODO: error messages and incorrect input, maybe CLI parser
-# TODO: print out data nicely, maybe add location-based message
-# TODO: open web browser with location on maps
+# main code execution
+parsedInfo = GetInfo(query)
+# print(parsedInfo)
+PrintOutput(parsedInfo)
+# OpenMap(parsedInfo)
